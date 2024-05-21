8f02ab626efddb55d33cc6e0e9f92fa7358d7eaf
Anuj Koirala <anujkoirala01@gmail.com>
2024-05-21 09:10:34 +0545

    readUser

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,6 @@
 import { User } from "../model/model.js";
 import bcrypt from "bcrypt";
+import { sendEmail } from "../utils/sendEmail.js";
 
 export let createUser = async (req, res) => {
   let userData = req.body;
@@ -8,9 +9,31 @@ export let createUser = async (req, res) => {
     let hashPassword = await bcrypt.hash(password, 10); // 10 --> SaltRound
     userData.password = hashPassword;
     let result = await User.create(userData);
+    await sendEmail({
+      from: "AK",
+      to: [req.body.email],
+      subject: "Email Verification",
+      html: `<h1>You have been successfully registered.</h1>`,
+    });
     res.json({
       success: true,
       message: "User created successfully",
+      result: result,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export let readUser = async (req, res) => {
+  try {
+    let result = await User.find({});
+    res.json({
+      success: true,
+      message: "User Read Successfully",
       result: result,
     });
   } catch (error) {
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser } from "../controller/userController.js";
+import { createUser, readUser } from "../controller/userController.js";
+import { User } from "../model/model.js";
 
 let userRouter = Router();
 
-userRouter.route("/").post(createUser);
+userRouter.route("/").post(createUser).get(readUser);
 
 export default userRouter;
 
